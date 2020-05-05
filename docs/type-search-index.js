e5bcda74759d5ecc5467c6d416305af90fe669d2
osamusasa <diamond.dust1997@gmail.com>
2020-05-05 20:49:58 +0900

    update Javadoc

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"Main","h":"","u":""},{"l":"TiledMap","h":"","u":""},{"l":"TiledMapView","h":"","u":""},{"l":"すべてのクラス","h":"","u":"allclasses-index.html"}]
+typeSearchIndex = [{"l":"BitmapTile","h":"","u":""},{"l":"Main","h":"","u":""},{"l":"SingleSquareTile","h":"","u":""},{"l":"TiledMap","h":"","u":""},{"l":"TiledMapView","h":"","u":""},{"l":"すべてのクラス","h":"","u":"allclasses-index.html"}]
